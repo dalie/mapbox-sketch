6a18e5734494879c526ebb15a007e0c24165cb44
Dominic Alie <dalie@arturo.ai>
2022-08-31 22:29:10 -0400

    Basic structure

---
+++ packages/ngx-mapbox-sketch/README.md
@@ -0,0 +1,7 @@
+# ngx-mapbox-sketch
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test ngx-mapbox-sketch` to execute the unit tests.
---
+++ packages/react-mapbox-sketch/src/lib/react-mapbox-sketch.tsx
@@ -0,0 +1,14 @@
+import styles from './react-mapbox-sketch.module.scss';
+
+/* eslint-disable-next-line */
+export interface ReactMapboxSketchProps {}
+
+export function ReactMapboxSketch(props: ReactMapboxSketchProps) {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to ReactMapboxSketch!</h1>
+    </div>
+  );
+}
+
+export default ReactMapboxSketch;
